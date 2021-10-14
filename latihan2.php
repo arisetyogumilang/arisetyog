8f448cb20d4e04984d134659153e4078d1971e19
arisetyogumilang <61956150+arisetyogumilang@users.noreply.github.com>
2021-10-14 23:43:47 +0700

    5. STRUKTUR KENDALI

---
+++ latihan2.php
@@ -0,0 +1,37 @@
+<?php
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+// $x = 30;
+// if ($x < 20) {
+//     echo "benar";
+// } else {
+//     echo "salah";
+// }
+
+// $x = 2;
+// if( $x < 20 ) {
+// 	echo "benar";
+// } else if( $x == 20 ) {
+// 	echo "bingo!";
+// } else {
+// 	echo "salah";
+// }
+
+// $x = 2;
+// echo $x < 20 ? 'benar' : 'salah',
+
+// $x = 30;
+// switch ($x) {
+//     case $x < 20:
+//         echo "benar";
+//         break;
+//     case $x == 20:
+//         echo "benar";
+//         break;
+//     default:
+//         echo "salah";
+// }
